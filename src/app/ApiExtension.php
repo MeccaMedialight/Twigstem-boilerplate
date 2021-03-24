26d9b43e6010bef1e4dc3fc1c5afac3dc46803ca
Luke Wigley <luke@meccamedialight.com.au>
2021-03-24 17:09:46 +1100

    wip mc-wipface

--- src/app/ApiExtension.php
+++ src/app/ApiExtension.php
@@ -10,20 +10,44 @@ use Twig\TwigFunction;
 
 class ApiExtension //extends AbstractExtension
 {
-    public function getFilters()
-    {
-        return [
-            new TwigFunction('area', [$this, 'calculateArea']),
+    private $filters;
+
+    public function __construct()  {
+        $this->filters = [
+            'randomise' =>  [$this, 'randomise'],
+            'limit' =>  [$this, 'limit'],
         ];
     }
 
-    public function randomise(array &$data)
-    {
-        shuffle($data);
+    public function applyFilter($filter, &$data){
+
+        if (is_array($filter)){
+            $name = array_shift($filter);
+
+        } else {
+            $name = $filter;
+            $filter = array();
+        }
+
+        if (isset($this->filters[$name])){
+            //echo "calling $name ...";
+            $data = call_user_func($this->filters[$name], $data, $filter);
+
+        } else {
+            //echo "Filter $name not found";
+        }
+
     }
 
-    public function limit(array &$data, int $maxlength)
+
+    public function randomise(array $data, $args = array())
     {
-        shuffle($data);
+        shuffle($data['movies']);
+        return $data;
+    }
+
+    public function limit(array $data, $args = array())
+    {
+        return array_slice($data, 0, 8);
     }
 }
