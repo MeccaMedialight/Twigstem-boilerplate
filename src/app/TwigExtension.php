705c1345f1a52d16435aa93fa871704de26da952
Luke Wigley <luke@meccamedialight.com.au>
2021-03-29 13:33:35 +1100

    added some more sections

--- src/app/TwigExtension.php
+++ src/app/TwigExtension.php
@@ -13,12 +13,26 @@ class TwigExtension extends AbstractExtension
     public function getFunctions()
     {
         return [
-            new TwigFunction('area', [$this, 'calculateArea']),
+            new TwigFunction('listAllSections', [$this, 'listAllSections']),
         ];
     }
 
-    public function calculateArea(int $width, int $length)
+    /**
+     * Sample function to list all the twigs in a directory
+     *
+     * @param string $twigDir name of directory in the views directory
+     * @return array
+     */
+    public function listAllSections( $twigDir = '_sections')
     {
-        return $width * $length;
+        $srcDir = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $twigDir;
+        $files = glob($srcDir . '/*.twig');
+
+        $out = [];
+        foreach($files as $f){
+            $out[] = basename($f, '.twig');
+        }
+
+        return $out;
     }
 }
